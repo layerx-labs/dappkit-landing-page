c2cafce404de0621d6363e5e11aef4fd7c858869
Henrique Macedo <henrique@hey.com>
2022-05-15 20:46:01 +0100

    Update copy

--- src/components/design/hero/index.js
+++ src/components/design/hero/index.js
@@ -1,19 +1,15 @@
 import ButtonLink from "../button-link";
 import * as Styles from "./styles";
 
-function Hero() {
+function Hero(props) {
+  const { getStartedUrl, docsUrl, children } = props;
+
   return (
     <Styles.Wrapper>
-      <h1>
-        <span>Your Web3 project</span>
-        <br /> <span>3 lines of code away</span>
-      </h1>
-      <span>
-        dappKit is a powerful SDK that allows you to develop beautiful web3 apps
-      </span>
+      {children}
       <Styles.ActionButtons>
-        <ButtonLink color="dark" url="#0" value="Get Started" />
-        <ButtonLink url="#0" value="Documentation" />
+        <ButtonLink color="dark" url={getStartedUrl} value="Get Started" />
+        <ButtonLink url={docsUrl} value="Documentation" />
       </Styles.ActionButtons>
     </Styles.Wrapper>
   );
--- src/components/intro.js
+++ src/components/intro.js
@@ -1,7 +1,17 @@
 import Hero from "./design/hero";
 
 function Intro() {
-  return <Hero />;
+  return (
+    <Hero getStartedUrl="#0" docsUrl="#0">
+      <h1>
+        <span>Your Web3 project</span>
+        <br /> <span>3 lines of code away</span>
+      </h1>
+      <span>
+        dappKit is a powerful SDK that allows you to develop beautiful web3 apps
+      </span>
+    </Hero>
+  );
 }
 
 export default Intro;
