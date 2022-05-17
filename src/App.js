1eaf4f9140539a23676548a24a292c03c6caece4
Henrique Macedo <henrique@hey.com>
2022-05-17 11:19:03 +0100

    Add parallax effect on pricing section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useRef } from "react";
+import styled from "styled-components";
+import { Parallax } from "@react-spring/parallax";
 import NavMenu from "./components/nav-menu";
 import Intro from "./components/intro";
 import Examples from "./components/examples";
@@ -9,20 +12,33 @@ import Pricing from "./components/pricing";
 import Projects from "./components/projects";
 import Footer from "./components/footer";
 
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+
+  > div > div {
+    overflow: initial !important;
+  }
+`;
+
 function App() {
+  const parallax = useRef(null);
   return (
-    <>
-      <NavMenu />
-      <Intro />
-      <Examples />
-      <Sdk />
-      <Features id="features" />
-      <Templates id="templates" />
-      <Bounties />
-      <Pricing id="pricing" />
-      <Projects />
-      <Footer />
-    </>
+    <Wrapper>
+      <Parallax ref={parallax} pages={0}>
+        <NavMenu />
+        <Intro />
+        <Examples />
+        <Sdk />
+        <Features id="features" />
+        <Templates id="templates" />
+        <Bounties />
+        <Pricing id="pricing" />
+        <Projects />
+        <Footer />
+      </Parallax>
+    </Wrapper>
   );
 }
 
--- src/components/design/price/index.js
+++ src/components/design/price/index.js
@@ -1,3 +1,4 @@
+import { ParallaxLayer } from "@react-spring/parallax";
 import Section from "../section";
 import { Coin1, Coin2, Coin3, Coin4, Coin5, Coin6 } from "../../../utils/coins";
 import * as Styles from "./styles";
@@ -7,12 +8,24 @@ function Price(props) {
 
   return (
     <Styles.Wrapper>
-      <Coin1 />
-      <Coin2 />
-      <Coin3 />
-      <Coin4 />
-      <Coin5 />
-      <Coin6 />
+      <ParallaxLayer offset={0.15} speed={0.05}>
+        <Coin1 />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.3} speed={0.1}>
+        <Coin2 />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.5} speed={0.15}>
+        <Coin3 />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.6} speed={0.2}>
+        <Coin4 />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.05} speed={0.25}>
+        <Coin5 />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.1} speed={0.3}>
+        <Coin6 />
+      </ParallaxLayer>
 
       <Section id={id} color="green">
         <h2>{title}</h2>
