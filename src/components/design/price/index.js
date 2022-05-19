5232f01337b2a08bf497f12bd149a96b7757298f
Henrique Macedo <henrique@hey.com>
2022-05-19 15:03:02 +0100

    Fix build

--- src/components/design/price/index.js
+++ src/components/design/price/index.js
@@ -1,6 +1,6 @@
 import { InView } from "react-intersection-observer";
 import Section from "../section";
-import { Coin1, Coin2, Coin3, Coin4, Coin5, Coin6 } from "../../../utils/coins";
+import { Coin1, Coin2, Coin3 } from "../../../utils/coins";
 import * as Styles from "./styles";
 
 function Price(props) {
