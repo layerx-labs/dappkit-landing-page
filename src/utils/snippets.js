07a9b9c310b9f3ef3d3351f24eeea41c22d086f6
Henrique Macedo <henrique@hey.com>
2022-05-20 15:13:11 +0100

    Update snippets.js

--- src/utils/snippets.js
+++ src/utils/snippets.js
@@ -1,5 +1,5 @@
 export const nft =
-  'import { Erc721Standard } from "@taikai/dappkit";\r\n\r\nconst nftToken = new Erc721Standard(\r\n  {\r\n    web3Host: "https://mainnet.infura.io/v3/",\r\n    privateKey: "*"\r\n  }, "0xContractAddress"\r\n);\r\n\r\nawait nftToken.start();\r\nawait nftToken.mint("0xWalletAddress", "tokenId-1");';
+  'import { Erc721Standard } from "@taikai/dappkit";\r\n\r\nconst nftToken = new Erc721Standard(\r\n  { web3Host: "https://mainnet.infura.io/v3/",\r\n    privateKey: "*"\r\n  }, "0xContractAddress"\r\n);\r\n\r\nawait nftToken.start();\r\nawait nftToken.mint("0xWalletAddress", "tokenId-1");';
 
 export const crypto =
   'import { ERC20 } from "@taikai/dappkit";\r\n\r\nconst erc20 = new ERC20(\r\n  { web3Host: "https://mainnet.infura.io/v3/",\r\n    privateKey: "*"\r\n  }, "0xContractAddress"\r\n);\r\n\r\nawait erc20.start();\r\nawait erc20.transferTokenAmount("0xWalletAddress", 100);\r\n';
